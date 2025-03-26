57cdfb7295dd236a4544ef3fb309d98851aa693f
isasoutos <isasoutos123@gmail.com>
2025-03-26 16:45:22 +0000

    Adicionado gravar e ler arquivo

---
+++ funcao.h
@@ -0,0 +1,28 @@
+typedef struct 
+{
+    char nome[20];
+    int prioridade;
+    int duracao;
+} Tarefa;
+
+typedef struct 
+{
+    Tarefa *vetor[100];
+    int qtde;
+} Lista;
+
+int inserir_tarefa(Lista *, Tarefa *);
+
+Tarefa *cria_tarefa();
+
+void mostra_tarefas(Lista *);
+
+int menu();
+
+void remover_tarefa(Lista *);
+
+void consultar_tarefa(Lista *);
+
+void gravar_arquivos(Lista *lista);
+
+void ler_arquivos(Lista *);
